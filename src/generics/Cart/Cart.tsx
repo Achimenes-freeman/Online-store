b0ff628e2c9c86d62492981755adc205be0463b7
iSvitka <svitka.ilya@gmail.com>
2022-12-23 18:40:36 +0300

    refactor: adjust Cart.tsx and scss files

--- src/generics/Cart/Cart.tsx
+++ src/generics/Cart/Cart.tsx
@@ -3,14 +3,11 @@ import { CartType } from './types'
 import styles from './styles.module.scss'
 
 export default function Cart({productsAmount = 0, callback}:CartType) {
-    let amount = '';
-    if(productsAmount >= 100) {
-        amount = `99+`
-    } else amount = productsAmount.toString();
+    const amount = productsAmount >= 100 ? '99+' : productsAmount;
     return (
         <button type='button' className={styles.cartButton} onClick={callback}>
             <img className={styles.cartImg} src={cartIcon} alt="Cart" />
-            <span className={productsAmount ? styles.cartAmountVisible : styles.cartAmountHidden}>{amount}</span>
+            {!! productsAmount && <span className={styles.cartAmountVisible}>{amount}</span>}
         </button>
     )
 }
