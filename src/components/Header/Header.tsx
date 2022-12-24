c4af4ab5f5fef54dff5f364a6026db9936da03fa
iSvitka <svitka.ilya@gmail.com>
2022-12-24 13:58:15 +0300

    feat: add styles and logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,28 @@
+import Cart from '../../generics/Cart/Cart';
 import styles from './styles.module.scss';
 import { HeaderType } from './types';
 
 export default function Header({productsCount, productsTotalPrice}:HeaderType) {
+    let totalCount: string | number;
+    if(productsTotalPrice) {
+        totalCount = productsTotalPrice;
+        if(productsTotalPrice >= 1000) {
+            totalCount = `${Math.round(productsTotalPrice / 100) / 10}K`
+        }
+        if(productsTotalPrice >= 1000000) {
+            totalCount = `${Math.round(productsTotalPrice / 10000) / 100}M`;
+        }
+    } else totalCount = 0;
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <div>Online Store</div>
-                <div>Total Count: €{productsTotalPrice}</div>
-                <button type="button">Cart: {productsCount}</button>
+                <button type='button' className={styles.headerLogo}>
+                    <span className={styles.headerLogoSpan1}>Online</span>
+                    <span className={styles.headerLogoSpan2}>Store</span>
+                </button>
+                <div className={styles.cartTotal}>Total Count: <span className={styles.cartTotalSpan}>€{totalCount}</span></div>
+                <Cart productsAmount={productsCount}/>
             </div>
         </header>
     )
