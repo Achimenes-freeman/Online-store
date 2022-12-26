01054068c5f3d5ac497d3b8a9fb96bee6909929f
Achimenes-freeman <gangsta10ip@mail.ru>
2022-12-26 18:38:34 +0300

    feat: refacroting class names

--- src/generics/SortInput/SortInput.tsx
+++ src/generics/SortInput/SortInput.tsx
@@ -5,21 +5,21 @@ function SortInput({ callback }: SortInputProps) {
     return (
         <div className={styles.SelectWrapper}>
             <select
-                className={styles.Select}
+                className={styles.select}
                 name="sorting"
                 defaultValue="high-rate"
                 onChange={callback}
             >
-                <option className={styles.Option} value="high-price">
+                <option className={styles.option} value="high-price">
                     Ascending price
                 </option>
-                <option className={styles.Option} value="low-price">
+                <option className={styles.option} value="low-price">
                     Price descending
                 </option>
-                <option className={styles.Option} value="high-rate">
+                <option className={styles.option} value="high-rate">
                     Highest rating
                 </option>
-                <option className={styles.Option} value="low-rate">
+                <option className={styles.option} value="low-rate">
                     Lowest rating
                 </option>
             </select>
