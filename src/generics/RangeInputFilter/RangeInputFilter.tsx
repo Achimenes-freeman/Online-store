02e3e022010abb8d9eee5b19fe8ab1b5ef8663f0
iSvitka <svitka.ilya@gmail.com>
2022-12-27 12:53:49 +0300

    refactor: refactor RangeInputFilter.tsx

--- src/generics/RangeInputFilter/RangeInputFilter.tsx
+++ src/generics/RangeInputFilter/RangeInputFilter.tsx
@@ -3,28 +3,28 @@ import { GenericRangeInputFilter } from "./types";
 import styles from "./styles.module.scss";
 
 export default function RangeInputFilter({min, max, callback}: GenericRangeInputFilter) {
-    const [value1, setValue1] = useState(min);
-    const [value2, setValue2] = useState(max);
+    const [minValue, setMinValue] = useState(min);
+    const [maxValue, setMaxValue] = useState(max);
     const [fillRange, setFillRange] = useState({
-        background: `linear-gradient(to right, #dae4ff ${(value1 / max) * 100}% , #0c75ff ${(value1 / max) * 100}% , #0c75ff ${(value2 / max) * 100}%, #dae4ff ${(value2 / max) * 100}%)`
+        background: `linear-gradient(to right, #dae4ff ${(minValue / max) * 100}% , #0c75ff ${(minValue / max) * 100}% , #0c75ff ${(maxValue / max) * 100}%, #dae4ff ${(maxValue / max) * 100}%)`
     });
     
-    const onChange1 = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeMin = (event:React.ChangeEvent<HTMLInputElement>) => {
         if(event.target.value) {
             let newValue = Number(event.target.value);
-            const per1 = (newValue / Number(max)) * 100;
-            const per2 = (value2 / Number(max)) * 100;
+            const perMin = (newValue / Number(max)) * 100;
+            const perMax = (maxValue / Number(max)) * 100;
             if(newValue === max) {
                 newValue = max - 1;
             }
-            if((value2 - newValue) <= 0) {
-                setValue2(newValue + 1)
+            if((maxValue - newValue) <= 0) {
+                setMaxValue(newValue + 1)
             }
             
-            setValue1(Number(newValue));
+            setMinValue(Number(newValue));
 
             setFillRange({
-                background: `linear-gradient(to right, #dae4ff ${per1}% , #0c75ff ${per1}% , #0c75ff ${per2}%, #dae4ff ${per2}%)`
+                background: `linear-gradient(to right, #dae4ff ${perMin}% , #0c75ff ${perMin}% , #0c75ff ${perMax}%, #dae4ff ${perMax}%)`
             })
         }
         
@@ -32,18 +32,18 @@ export default function RangeInputFilter({min, max, callback}: GenericRangeInput
             callback();
         }
     }
-    const onChange2 = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const onChangeMax = (event:React.ChangeEvent<HTMLInputElement>) => {
         if(event.target.value) {
             let newValue = Number(event.target.value);
-            const per1 = (value1 / Number(max)) * 100;
+            const per1 = (minValue / Number(max)) * 100;
             const per2 = (newValue / Number(max)) * 100;
             if(newValue === min) {
                 newValue = min + 1;
             }
-            if((newValue - value1) <= 0) {
-                setValue1(newValue - 1);
+            if((newValue - minValue) <= 0) {
+                setMinValue(newValue - 1);
             }
-            setValue2(newValue);
+            setMaxValue(newValue);
 
             setFillRange({
                 background: `linear-gradient(to right, #dae4ff ${per1}% , #0c75ff ${per1}% , #0c75ff ${per2}%, #dae4ff ${per2}%)`
@@ -57,12 +57,12 @@ export default function RangeInputFilter({min, max, callback}: GenericRangeInput
     return (
         <>
             <div className={styles.spanContainer}>
-                <span>{value1}</span>—<span>{value2}</span>
+                <span>{minValue}</span>—<span>{maxValue}</span>
             </div>
             <div className={styles.inputContainer}>
                 <div className={styles.sliderTrack} style={fillRange}/>
-                <input className={styles.input} type="range" min={min} max={max} onChange={onChange1} value={value1} />
-                <input className={styles.input} type="range" min={min} max={max} onChange={onChange2} value={value2} />
+                <input className={styles.input} type="range" min={min} max={max} onChange={onChangeMin} value={minValue} />
+                <input className={styles.input} type="range" min={min} max={max} onChange={onChangeMax} value={maxValue} />
             </div>
         </>
     )
